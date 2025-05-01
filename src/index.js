6a1a9d7917774689cbe094de248b55b69f1d90d2
Siddesh-2004 <siddeshpoojary2004@gmail.com>
2025-05-01 17:38:27 +0530

    vedio 9

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import connectDB from './db/index.js'
 dotenv.config({
     path:'./env'
 })
-
 connectDB()
+ .then(() => {
+    console.log("Database connected successfully");
+
+    app.listen(process.env.PORT||8000 , () => {
+        console.log("App is listening on port", process.env.PORT);
+    });
+ })
+ .catch((error)=>{
+    console.error("connection failed",error)
+ })
+
+
 
 
 
